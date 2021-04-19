63621a75f3b173e4f281bf6f8b0d42ed7f8ca0dd
Chris Tack <chris_tack@mac.com>
2021-04-18 21:14:02 -0700

    Fix typos

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,32 +1,31 @@
 exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPages } = actions
-
-  const sectionTemplate = require.resolve(`./src/templates/sectionTemplate.js`)
-
+  const { createPage } = actions
+  const blogPostTemplate = require.resolve(`./src/templates/blogTemplate.js`)
   const result = await graphql(`
-    allMarkdownRemark(
-      limit: 1000
-    ) {
-      edges {
-        node {
-          frontmatter {
-            slug
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
           }
         }
       }
     }
   `)
-
   // Handle errors
   if (result.errors) {
     reporter.panicOnBuild(`Error while running GraphQL query.`)
     return
   }
-
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPages({
+    createPage({
       path: node.frontmatter.slug,
-      component: sectionTemplate,
+      component: blogPostTemplate,
       context: {
         // additional data can be passed via context
         slug: node.frontmatter.slug,
